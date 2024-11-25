cbe75092740921bd9afd23604a446ef757db430e
Mayank Saini <mayanksaini4455@gmail.com>
2024-11-25 11:16:22 +0530

    project-one Added Create-user route

--- project-one/src/user/user.controller.ts
+++ project-one/src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private user: UserService){}
+    constructor(private user: UserService) { }
 
     @Get('req')
-    getUserReq(@Req() req: Request): string{
+    getUserReq(@Req() req: Request): string {
         return this.user.getUserReq(req);
+    }
+
+    @Get(':id')
+    getUser(@Param() param: any): string {
+        const { id } = param;
+        return this.user.getUser(id);
+    }
+
+    @Post()
+    createUser(@Body() body: any): string {
+        return this.user.createUser(body);
     }
 }
--- project-one/src/user/user.service.ts
+++ project-one/src/user/user.service.ts
@@ -2,9 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-    
-    public getUserReq(req: Request){
-        console.log('-----------------> REQ: ',req);
+
+    public getUserReq(req: Request) {
+        console.log(' getUserReq -----------------> REQ: ', req);
         return "Request printed succesffuly";
+    }
+
+    public getUser(id: string) {
+        console.log(' getUser -----------------> ID: ', id);
+        return "User Id: " + id;
+    }
+
+    public createUser(user: any) {
+        console.log(' createUser -----------------> USER: ', user);
+        return "User Created: " + user;
     }
 }
