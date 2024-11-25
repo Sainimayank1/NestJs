85393cb92bb37f520060a1c81a9506c6f152e663
Mayank Saini <mayanksaini4455@gmail.com>
2024-11-25 13:49:37 +0530

    project-one Udpdated logger provider just to user module

--- project-one/src/app.module.ts
+++ project-one/src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { UserModule } from './user/user.module';
-import { LoggerService } from './logger/logger.service';
 
 @Module({
   imports: [HealthCheckModule, UserModule],
   controllers: [AppController],
-  providers: [AppService, LoggerService],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
